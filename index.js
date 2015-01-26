7f2262fd2741d940a7c4efa1aed1dd406be26214
andy <andyjiang@gmail.com>
2015-01-25 18:42:23 -0800

    did stuff

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
-module.exports = require('./lib/index');
+var dir = require('node-dir');
+
+dir.paths(__dirname, true, function(err, paths) {
+  if (err) throw err;
+  console.log('paths:\n',paths);
+});
